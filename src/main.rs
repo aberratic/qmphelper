a40e38e84413495a1650f63256548462db11b80c
Stefan Luecke <aberratic@aberrational.org>
2023-10-29 10:59:06 +0100

    Add support for block-comit and block-job-complete

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use clap::{Args, Parser, Subcommand};
-use futures::StreamExt;
-use subcommands::query_block_stats::QueryBlockStatsArguments;
+use clap::{Parser, Subcommand};
+use subcommands::{
+    block_commit::BlockCommitArguments, block_job_complete::BlockJobCompleteArguments,
+    query_block_stats::QueryBlockStatsArguments,
+};
 use tokio::io;
-use tracing::{debug, error, info, instrument, span, trace, warn, Level};
+use tracing::{debug, error};
 use tracing_subscriber;
 mod subcommands;
 
@@ -16,8 +18,21 @@ struct Arguments {
 
 #[derive(Subcommand, Debug, Clone)]
 enum Subcommands {
+    /// Live commit of data from overlay image nodes into backing nodes - i.e., writes
+    /// data between ‘top’ and ‘base’ into ‘base’.
+    /// If top == base, that is an error. If top has no overlays on top of it, or if it
+    /// is in use by a writer, the job will not be completed by itself. The user needs to
+    /// complete the job with the block-job-complete command after getting the ready event.
+    /// If the base image is smaller than top, then the base image will be resized to
+    /// be the same size as top. If top is smaller than  the base image, the base will
+    /// not be truncated. If you want the base image size to match the size of the smaller
+    /// top, you can safely truncate it yourself once the commit operation successfully
+    /// dscompletes.
+    BlockCommit(BlockCommitArguments),
     /// Query the BlockStats for all virtual block devices
     QueryBlockStats(QueryBlockStatsArguments),
+    QueryBlockJobs,
+    BlockJobComplete(BlockJobCompleteArguments),
 }
 
 macro_rules! handle_errors {
@@ -66,9 +81,46 @@ pub async fn main() -> io::Result<()> {
     debug!("Spawned QMP stream");
 
     match args.command {
+        Subcommands::BlockCommit(args) => {
+            let qmp_onerror: Option<qapi::qmp::BlockdevOnError> = match args.on_error {
+                Some(on_error) => Some(on_error.into()),
+                None => None,
+            };
+            let query = qapi::qmp::block_commit {
+                device: args.device,
+                job_id: args.job_id,
+                base_node: args.base_node,
+                base: args.base,
+                top_node: args.top_node,
+                top: args.top,
+                backing_file: args.backing_file,
+                speed: args.speed,
+                on_error: qmp_onerror,
+                filter_node_name: args.filter_node_name,
+                auto_finalize: args.auto_finalize,
+                auto_dismiss: args.auto_dismiss,
+            };
+            debug!("Executing query: {:#?}", query);
+            let response = qmp.execute(query).await?;
+            println!("{:#?}", response);
+        }
         Subcommands::QueryBlockStats(args) => {
             let query = qapi::qmp::query_blockstats {
                 query_nodes: args.query_nodes,
+            };
+            debug!("Executing query: {:#?}", query);
+            let response = qmp.execute(query).await?;
+            println!("{:#?}", response);
+        }
+        Subcommands::QueryBlockJobs => {
+            let query = qapi::qmp::query_block_jobs {};
+            debug!("Executing query: {:#?}", query);
+            let response = qmp.execute(query).await?;
+            println!("{:#?}", response);
+        }
+        Subcommands::BlockJobComplete(args) => {
+            let query = qapi::qmp::block_job_complete {
+                device: args.device,
             };
             debug!("Executing query: {:#?}", query);
             let response = qmp.execute(query).await?;
--- src/subcommands/mod.rs
+++ src/subcommands/mod.rs
@@ -1,1 +1,3 @@
+pub mod block_commit;
+pub mod block_job_complete;
 pub mod query_block_stats;
